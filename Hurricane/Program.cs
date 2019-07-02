f2cf99ee7d3877595566d1895f21e7c717b76eea
pavelsokolovan <you@example.com>
2019-07-02 22:12:44 +0300

    GetCredentials moved to WebUtils

--- Hurricane/Program.cs
+++ Hurricane/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Serilog;
 using System;
-using System.Configuration;
 
 namespace Hurricane
 {
@@ -12,7 +11,7 @@ namespace Hurricane
         {
             PrepareLogger();
 
-            (string login, string password) = GetCredentials();
+            (string login, string password) = WebUtils.GetCredentials();
 
             Bot bot = new Bot();
             bot.OnMessage += BotOnMessage;
@@ -48,46 +47,6 @@ namespace Hurricane
                         .WriteTo.Console()
                         .WriteTo.File("logfile.log")
                         .CreateLogger();
-        }
-
-        private static (string login, string password) GetCredentials()
-        {
-            Console.Write("login: ");
-            string login = Console.ReadLine();
-            Console.Write("password: ");
-            string password = ReadPassword();
-            Console.Clear();
-
-            return (login, password);
-        }
-
-        private static string ReadPassword()
-        {
-            string password = string.Empty;
-
-            do
-            {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-                {
-                    password += key.KeyChar;
-                    Console.Write("*");
-                }
-                else
-                {
-                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
-                    {
-                        password = password.Substring(0, (password.Length - 1));
-                        Console.Write("\b \b");
-                    }
-                    else if (key.Key == ConsoleKey.Enter)
-                    {
-                        break;
-                    }
-                }
-            } while (true);
-
-            return password;
-        }
+        }   
     }
 }
---
+++ Hurricane/WebUtils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Hurricane
+{
+    public class WebUtils
+    {
+        public static (string login, string password) GetCredentials()
+        {
+            Console.Write("login: ");
+            string login = Console.ReadLine();
+            Console.Write("password: ");
+            string password = ReadPassword();
+            Console.Clear();
+
+            return (login, password);
+        }
+
+        private static string ReadPassword()
+        {
+            string password = string.Empty;
+
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                    {
+                        password = password.Substring(0, (password.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+
+            return password;
+        }
+    }
+}
